1c8118cd971d325cdffb166b1103c8c8cffa346a
Andrés <Andrs@DESKTOP-4J15R21>
2021-01-21 14:13:39 +0100

    commit del ejercicios de maximos con fallo en los maximos, ha falta de ver el video

--- src/ejerciciosjavanv1/EjerciciosJavaNV1.java
+++ src/ejerciciosjavanv1/EjerciciosJavaNV1.java
@@ -5,12 +5,16 @@
  */
 package ejerciciosjavanv1;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Andrés
  */
 public class EjerciciosJavaNV1 {
     
+    /*
+    Ejemplo de como hacer un array y que busque el numero mayor de la lista.
      //Declaro un array de integers para hacer pruebas.
     int[] listaNumeros = {333, 31, 200, 20, 5, 99};
     
@@ -24,15 +28,40 @@ public class EjerciciosJavaNV1 {
         }
         return auxiliar;
     }
-
+    */
     /**
      * @param args the command line arguments
      */
+    /*
     public static void main(String[] args) {
         EjerciciosJavaNV1 ejercicio = new EjerciciosJavaNV1();
         System.out.println(ejercicio.maximo(ejercicio.listaNumeros));
         
         
     }
+    */
+     /**
+     * @param args the command line arguments
+     */
+    int[] listaNumeros = {50, 31, 27, 2, 5, 99};
     
+    private int[] maximos (int[] lista) {
+        int[] auxiliar = {lista[0], lista[0]};
+        
+        for (int i = 0; i < lista.length; i++) {
+           if (auxiliar[0] < lista[1]) {
+               auxiliar[0] = lista[1];   
+            } 
+           else if (auxiliar [1] < lista[1]) {
+               auxiliar[1] = lista[1];
+           }
+        }
+        
+        return auxiliar;
+    }
+    
+     public static void main(String[] args) {
+        EjerciciosJavaNV1 ejercicio = new EjerciciosJavaNV1();
+        System.out.println(Arrays.toString(ejercicio.maximos(ejercicio.listaNumeros)));
+     }
 }
