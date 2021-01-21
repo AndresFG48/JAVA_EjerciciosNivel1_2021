185e61923fedf505ca654c34e465e3a87c686640
Andrés <Andrs@DESKTOP-4J15R21>
2021-01-21 13:15:08 +0100

    primer commit de los ejercicios java nivel 1.

---
+++ src/ejerciciosjavanv1/EjerciciosJavaNV1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjavanv1;
+
+/**
+ *
+ * @author Andrés
+ */
+public class EjerciciosJavaNV1 {
+    
+     //Declaro un array de integers para hacer pruebas.
+    int[] listaNumeros = {333, 31, 200, 20, 5, 99};
+    
+    public int maximo (int[] lista) {
+        int auxiliar = lista[0]; //El maximo va a ser el primer elemento del array.
+        
+        if (auxiliar < lista[1]) {
+            auxiliar = lista[1];
+        }
+        
+        return auxiliar;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EjerciciosJavaNV1 ejercicio = new EjerciciosJavaNV1();
+        System.out.println(ejercicio.listaNumeros);
+    }
+    
+}
