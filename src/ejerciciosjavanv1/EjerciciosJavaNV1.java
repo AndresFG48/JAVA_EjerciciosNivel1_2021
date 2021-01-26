a722c92ccfff7829738e542a1ebe46fab2a90234
Andrés <Andrs@DESKTOP-4J15R21>
2021-01-26 12:09:10 +0100

    commit del ejercicios de maximos, corregido los anteriores fallos y comprobado que lso maximos funcionan y ejercicio de palindromos.

--- src/ejerciciosjavanv1/EjerciciosJavaNV1.java
+++ src/ejerciciosjavanv1/EjerciciosJavaNV1.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
  * @author Andrés
  */
 public class EjerciciosJavaNV1 {
-    
+
     /*
     Ejemplo de como hacer un array y que busque el numero mayor de la lista.
      //Declaro un array de integers para hacer pruebas.
@@ -28,7 +28,7 @@ public class EjerciciosJavaNV1 {
         }
         return auxiliar;
     }
-    */
+     */
     /**
      * @param args the command line arguments
      */
@@ -39,30 +39,56 @@ public class EjerciciosJavaNV1 {
         
         
     }
-    */
-     /**
+     */
+    /**
      * @param args the command line arguments
      */
     int[] listaNumeros = {50, 31, 27, 2, 5, 99};
-    
-    private int[] maximos (int[] lista) {
-        int[] maximos = {0,0};
+
+    private int[] maximos(int[] lista) {
+        int[] maximos = {0, 0};
         for (int i = 0; i < lista.length; i++) {
-            if (lista [i] > maximos [0]) {
-                maximos [1] = maximos [0];
-                maximos [0] = lista [i];
+            if (lista[i] > maximos[0]) {
+                maximos[1] = maximos[0];
+                maximos[0] = lista[i];
+            } else if (lista[i] > maximos[1]) {
+                maximos[1] = lista[i];
             }
-            
-            else if (lista [i] > maximos [1]) {
-                maximos [1] = lista [i];
-            }
-        } 
-        
+        }
+
         return maximos;
     }
-    
-     public static void main(String[] args) {
+
+    /**
+     * Metódo para quitar los espacios en blanco a un String.
+     */
+    private String quitaEspaciosBlancos(String cadena) {
+
+        //Acaso hubo buhos aca.
+        //Acasohubobuhosaca.
+        String auxiliar = "";
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) != ' ') {
+                auxiliar = auxiliar + cadena.charAt(i);
+            }
+        }
+
+        return auxiliar;
+    }
+
+    public boolean esPalindromo(String frase) {
+
+        //Este programa devuelve verdadero si la frase tiene los mismos caracteres 
+        // de izquierda a derecha y de derecha a izquierda.
+        String auxiliar = quitaEspaciosBlancos (frase);
+        return false; //No es palindromo.
+    }
+
+    public static void main(String[] args) {
         EjerciciosJavaNV1 ejercicio = new EjerciciosJavaNV1();
-        System.out.println(Arrays.toString(ejercicio.maximos(ejercicio.listaNumeros)));
-     }
+        System.out.println("Los maximos son: " + Arrays.toString(ejercicio.maximos(ejercicio.listaNumeros)));
+
+        System.out.println("palíndromo: " + ejercicio.esPalindromo("Acaso hubo buhos aca"));
+        System.out.println("palíndromo: " + ejercicio.esPalindromo("esto no es"));
+    }
 }
